bfbd6370677f6af477aa6bafe20bb7f22634d2f0
Erik Arellano <erik.arellano_pm25256@cecyte.edu.mx>
2026-04-29 02:45:19 -0600

    Se agregaron los cambios en calculadoraBasica.py

--- ejercicios/erik-arellano/parcial2/calculadoraBasica.py
+++ ejercicios/erik-arellano/parcial2/calculadoraBasica.py
@@ -13,8 +13,6 @@ def multi (a,b):
 
 def div (a,b):
    return a / b
-
-
 #menu de opciones
 opcion = 0
 while(opcion != 5):
@@ -31,38 +29,27 @@ if opcion == 1:
    n1 = float(input("\nIngresa el primer numero a sumar: "))
    n2 = float(input("Ingresa el segundo valor a sumar: "))
    print(f"Resultado de la suma: {suma(n1,n2)}")
-
-
 elif opcion == 2:
    # Resta de dos numeros
    n1 = float(input("\nIngresa el minuendo: "))
    n2 = float(input("Ingresa el sustraendo: "))
    print(f"Diferencia de la resta: {resta(n1,n2)}")
-
-
 elif opcion == 3:
    # Multiplicacion de dos numeros
    n1 = float(input("\nIngresa el primer factor: "))
    n2 = float(input("Ingresa el segundo factor: "))
    print(f"Producto resultante: {multi(n1,n2)}")
-
-
 elif opcion == 4:
    # division de dos numeros
    n1 = float(input("\nIngresa el dividendo: "))
    n2 = float(input("Ingresa el divisor: "))
    if n2 != 0:
        print(f"Cociente resultante: {div(n1,n2)}")
-
-
 elif opcion == 5:
    # finalizacion del programa
    print("Programa finalizado...")
-
-
 else:
-   # mensaje de error por opcion no valida
-    "mensaje de opcion no valida, intenta nuevamente"
+    print( "mensaje de opcion no valida, intenta nuevamente")
 
 
 
